c9ca70f6b5f9acfc049bc3c9876034d0f4c225d1
martin <martin.zivojinovic@etu.u-bordeaux.fr>
2025-01-31 01:01:59 +0100

    episode 2

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ enum Rule {
 }
 
 impl Rule {
+    fn id(&self) -> usize {
+        (&*self as *const Rule) as usize
+    }
+
     fn check_contains_valid_atomic(&self, name_set: &HashSet<String>) -> Result<()> {
         match self {
             Self::Atomic(content) if !name_set.contains(content) => {
@@ -45,11 +49,14 @@ impl TryFrom<&str> for Box<Rule> {
             let mut operator_stack: Vec<char> = Vec::new();
             let mut output = Vec::new();
             let mut current_token = String::new();
+            let mut quotes = false;
             'main_loop: for c in expr.chars() {
-                if c == ' ' {
-                    continue;
+                if c == '\"' {
+                    quotes = !quotes;
                 }
                 match c {
+                    _ if quotes => current_token.push(c),
+                    ' ' => (),
                     '(' => {
                         push_token(&mut current_token, &mut output);
                         operator_stack.push(c)
@@ -148,6 +155,203 @@ struct Alphabet {
     alphabet: HashSet<char>,
 }
 
+enum NextCharResponse<'a> {
+    Reject,
+    Keep,
+    Out,
+    Replace(Vec<Candidate<'a>>),
+}
+
+#[derive(Debug)]
+struct Candidate<'a> {
+    rule: &'a Rule,
+    tokens: Vec<String>,
+    current_token: String,
+    path: Vec<Vec<&'a Rule>>,
+}
+
+impl<'a> Candidate<'a> {
+    fn get_candidates(rules: &'a HashMap<String, Box<Rule>>, rule: &'a Rule) -> Vec<Self> {
+        let mut res = HashMap::new();
+        Self::get_candidates_rec(&mut res, Vec::new(), String::new(), Vec::new(), rules, rule);
+        res.values().collect()
+    }
+
+    fn get_candidates_rec(
+        mut candidates: &mut HashMap<usize, Candidate<'a>>,
+        mut current_path: Vec<&'a Rule>,
+        current_token: String,
+        tokens: Vec<String>,
+        rules: &'a HashMap<String, Box<Rule>>,
+        rule: &'a Rule,
+    ) {
+        match rule {
+            Rule::Atomic(name) => match rules.get(name) {
+                Some(the_rule) => {
+                    if let Some(candidate) = candidates.get_mut(&the_rule.id()) {
+                        candidate.path.push(current_path)
+                    } else {
+                        Candidate::get_candidates_rec(
+                            candidates,
+                            current_path,
+                            current_token,
+                            tokens,
+                            rules,
+                            the_rule,
+                        )
+                    }
+                }
+                None => candidates.insert(
+                    rule.id(),
+                    Candidate {
+                        path: vec![current_path],
+                        rule,
+                        current_token,
+                        tokens,
+                    },
+                ),
+            },
+            Rule::Litteral(_) | Rule::EOW => candidates.insert(
+                rule.id(),
+                Candidate {
+                    path: vec![current_path],
+                    rule,
+                    current_token,
+                    tokens,
+                },
+            ),
+            Rule::Union([r1, r2]) => {
+                Candidate::get_candidates_rec(
+                    candidates,
+                    current_path.clone(),
+                    current_token.clone(),
+                    tokens.clone(),
+                    rules,
+                    r1,
+                );
+                Candidate::get_candidates_rec(
+                    candidates,
+                    current_path,
+                    current_token,
+                    tokens,
+                    rules,
+                    r2,
+                );
+            }
+            Rule::Chain([r1, _]) => {
+                current_path.push(rule);
+                Candidate::get_candidates_rec(
+                    candidates,
+                    current_path,
+                    current_token,
+                    tokens,
+                    rules,
+                    r1,
+                )
+            }
+        }
+    }
+
+    fn drain_current_token(&mut self) {
+        if !self.current_token.is_empty() {
+            self.tokens.push(self.current_token.drain(..).collect())
+        }
+    }
+
+    fn pop_path(&mut self, rules: &'a HashMap<String, Box<Rule>>) -> NextCharResponse<'a> {
+        let mut res = Vec::new();
+        for path in self.path {
+            match path.pop() {
+                Some(Rule::Chain([_, r2])) => {
+                    let mut map = HashMap::new();
+                    Self::get_candidates_rec(
+                        &mut map,
+                        self.path.drain(..).collect(),
+                        self.current_token.drain(..).collect(),
+                        self.tokens.drain(..).collect(),
+                        rules,
+                        &r2,
+                    );
+                    res.append(&mut map.values().collect())
+                }
+                None => (),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    fn get_next_candidates(
+        candidates: Vec<Candidate<'a>>,
+        c: char,
+        alphabets: &HashMap<String, Alphabet>,
+        rules: &'a HashMap<String, Box<Rule>>,
+    ) -> Result<Vec<Candidate<'a>>> {
+        let mut new_candidates = Vec::new();
+        for mut candidate in candidates.into_iter() {
+            match candidate.next_char(alphabets, rules, c)? {
+                NextCharResponse::Keep => new_candidates.push(candidate),
+                NextCharResponse::Replace(mut to_add) => new_candidates.append(&mut to_add),
+                NextCharResponse::Out => new_candidates.push(Candidate {
+                    rule: &Rule::EOW,
+                    path: Vec::new(),
+                    tokens: candidate.tokens.drain(..).collect(),
+                    current_token: candidate.current_token.drain(..).collect(),
+                }),
+                NextCharResponse::Reject => (),
+            }
+        }
+        Ok(new_candidates)
+    }
+
+    fn next_char(
+        &mut self,
+        alphabets: &HashMap<String, Alphabet>,
+        rules: &'a HashMap<String, Box<Rule>>,
+        c: char,
+    ) -> Result<NextCharResponse<'a>> {
+        Ok(match self.rule {
+            Rule::Atomic(name) => {
+                self.current_token.push(c);
+                if alphabets
+                    .get(name)
+                    .context("Alphebet should be there")?
+                    .alphabet
+                    .contains(&c)
+                {
+                    self.pop_path(rules)
+                } else {
+                    NextCharResponse::Reject
+                }
+            }
+            Rule::Litteral(lit) => {
+                self.current_token.push(c);
+                if *lit == self.current_token {
+                    self.drain_current_token();
+                    self.pop_path(rules)
+                } else if lit.starts_with(&self.current_token) {
+                    NextCharResponse::Keep
+                } else {
+                    NextCharResponse::Reject
+                }
+            }
+            Rule::EOW => {
+                if self.path.is_empty() && c != '\0' {
+                    return Ok(NextCharResponse::Reject);
+                }
+                self.drain_current_token();
+                let response = self.pop_path(rules);
+                match response {
+                    NextCharResponse::Replace(new_candidates) => NextCharResponse::Replace(
+                        Self::get_next_candidates(new_candidates, c, alphabets, rules)?,
+                    ),
+                    _ => response,
+                }
+            }
+            _ => unreachable!(),
+        })
+    }
+}
+
 #[derive(Debug)]
 struct ConfParser {
     state: State,
@@ -209,7 +413,7 @@ impl ConfParser {
         Ok(())
     }
 
-    fn parse(&mut self, content: String) -> Result<()> {
+    fn init(&mut self, content: String) -> Result<()> {
         for line in content
             .lines()
             .map(|line| line.trim())
@@ -224,6 +428,11 @@ impl ConfParser {
                 State::Nothing => bail!("Please declare the state before starting anything"),
             }?
         }
+        ensure!(
+            self.rules.contains_key("MAIN"),
+            "You should provide an entry point for the grammar"
+        );
+
         let name_set = self
             .rules
             .keys()
@@ -236,6 +445,35 @@ impl ConfParser {
         }
         Ok(())
     }
+
+    fn lex(&self, text: &str) -> Result<Vec<String>> {
+        let entry = self.rules.get("MAIN").context("MAIN is missing")?;
+        let mut candidates = Candidate::get_candidates(&self.rules, entry);
+        for c in text.chars() {
+            println!("{}", candidates.len());
+            candidates =
+                Candidate::get_next_candidates(candidates, c, &self.alphabets, &self.rules)?
+        }
+        // println!(
+        //     "{:?}",
+        //     candidates
+        //         .iter()
+        //         .map(|c| (c.rule, c.tokens.clone(), c.current_token.clone()))
+        //         .collect::<Vec<_>>()
+        // );
+
+        candidates.retain_mut(|candidate| {
+            matches!(
+                candidate.next_char(&self.alphabets, &self.rules, '\0'),
+                Ok(NextCharResponse::Out)
+            )
+        });
+        if candidates.is_empty() {
+            bail!("Syntax error")
+        } else {
+            Ok(candidates.pop().unwrap().tokens)
+        }
+    }
 }
 
 fn main() -> Result<()> {
@@ -245,7 +483,7 @@ fn main() -> Result<()> {
     f.read_to_string(&mut content)
         .context("Failed to read the content of the file")?;
     let mut parser = ConfParser::new();
-    parser.parse(content)?;
-
+    parser.init(content)?;
+    println!("{:?}", parser.lex("(((1+1)+100*zidjh))")?);
     Ok(())
 }
